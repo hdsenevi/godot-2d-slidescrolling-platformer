f9365c3bf0c06226450a3b0da1551c2a3044be38
Shanaka Senevirathne <shanaka36@gmail.com>
2020-09-30 17:50:24 +1000

    jumping

--- src/Actors/Actor.gd
+++ src/Actors/Actor.gd
@@ -1,9 +1,8 @@
 extends KinematicBody2D
 class_name Actor
 
-export var speed: = Vector2(300.0, 1000.0)
-export var gravity: = 3000.0
+const FLOOR_NORMAL: = Vector2.UP
+
+export var speed: = Vector2(800.0, 1000.0)
+export var gravity: = 4000.0
 var velocity: = Vector2.ZERO
-	
-func _physics_process(delta: float) -> void:
-	velocity.y += gravity * delta
